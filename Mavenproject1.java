d7cf0c9c747ce4a1d0542066a50475c025acbee7
Ahmad Sepriza <rizabtg7@gmail.com>
2025-09-08 09:54:38 +0800

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,220 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author User
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Mavenproject1 {
+    public static void main(String[] args) {
+        int pilihan;
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> daftarMahasiswa = new ArrayList<>();
+        daftarMahasiswa.add("Taufik Ramadhani");
+daftarMahasiswa.add("Nurhidayah");
+daftarMahasiswa.add("Isrina Luthfiah");
+daftarMahasiswa.add("Indah Putri Lestari");
+daftarMahasiswa.add("Grace Vies Angel");
+daftarMahasiswa.add("Adella Putri");
+daftarMahasiswa.add("Jen Agresia Misti");
+daftarMahasiswa.add("Angela Caroline Budiman");
+daftarMahasiswa.add("Nayla Camelia Indraswari");
+daftarMahasiswa.add("Narendra Augusta Srianandha");
+daftarMahasiswa.add("Nabila Imtiyaz Agustin");
+daftarMahasiswa.add("Dwi Pebriyanto Pradana");
+daftarMahasiswa.add("Hendri Zaidan Safitra");
+daftarMahasiswa.add("Zahraturramadhani");
+daftarMahasiswa.add("Putri Syafana Afrillia");
+daftarMahasiswa.add("Fathinatuz Zaina");
+daftarMahasiswa.add("Muhammad Fachri");
+daftarMahasiswa.add("Rahmad Ramadhan");
+daftarMahasiswa.add("Irvani Canlusty Missa");
+daftarMahasiswa.add("Zahra Aulia Rahmah");
+daftarMahasiswa.add("Aliyah Azzah Sekedang");
+daftarMahasiswa.add("Zelsya Rizqita Rahmadhini");
+daftarMahasiswa.add("Dilla Maharani");
+daftarMahasiswa.add("Deny Candra Kasuma");
+daftarMahasiswa.add("Ahmad Sepriza");
+daftarMahasiswa.add("Larry Polin Anugrah");
+daftarMahasiswa.add("Najmi Hafizh Mauludan Zain");
+daftarMahasiswa.add("Mochammad Rezky Ramadhan");
+daftarMahasiswa.add("Syauqi Etna Lazhuardhy");
+daftarMahasiswa.add("Muhammad Sadikin Samir");
+daftarMahasiswa.add("Maifariza Aulia Dyas");
+daftarMahasiswa.add("Muhammad Ilyasa' 'Izzuddin");
+daftarMahasiswa.add("Muhammad Reffi Fadillah");
+daftarMahasiswa.add("Muh Haikal Adis Yafiq");
+daftarMahasiswa.add("Sayid Rafi A'Thaya");
+daftarMahasiswa.add("Yardan Raditya Rafi' Widyadhana");
+daftarMahasiswa.add("Husaini Iyastama H");
+daftarMahasiswa.add("Rizqy");
+daftarMahasiswa.add("Dhita Olivia Ramadhayanti Kusuma");
+daftarMahasiswa.add("Shafa Rizqi Nur Wahidah");
+daftarMahasiswa.add("Ghani Mandala Putra");
+daftarMahasiswa.add("Fauzia Inanta Aurelia");
+daftarMahasiswa.add("Muhammad Rizky Febrianto");
+daftarMahasiswa.add("Chiqo Nanda Rial Pratama");
+daftarMahasiswa.add("Franklyn Galvin Lodo");
+daftarMahasiswa.add("Rini Wulandari");
+daftarMahasiswa.add("Rabiatul Hikmah");
+daftarMahasiswa.add("Dinathan Fahrezi");
+daftarMahasiswa.add("Nur Ihsan");
+daftarMahasiswa.add("Nadila Putri");
+daftarMahasiswa.add("Christian Amsal Asimaro Lumban Tobing");
+daftarMahasiswa.add("Prakasa Wira Mukti");
+daftarMahasiswa.add("Abdurrahman Al Farisy");
+daftarMahasiswa.add("Zidan Daffa Ramadhan");
+daftarMahasiswa.add("Azhaar Athahiroh");
+daftarMahasiswa.add("Syawe Manisha P. Siregar");
+daftarMahasiswa.add("Ghifari Al Azhar");
+daftarMahasiswa.add("Khairunisa Aprilia");
+daftarMahasiswa.add("Nayla Lelyanggraheni Hutomo");
+daftarMahasiswa.add("Zahra Aurellya Herdiansyah");
+daftarMahasiswa.add("Fikri Abiyyu Rahman");
+daftarMahasiswa.add("Darel Prasetya Fawwaz");
+daftarMahasiswa.add("Dharma Pala Candra");
+daftarMahasiswa.add("Satria Rajawali Ektya Antara");
+daftarMahasiswa.add("Asriah Ainun Fazah");
+daftarMahasiswa.add("Daffa Syahrandy Husain");
+daftarMahasiswa.add("Jemis Movid");
+daftarMahasiswa.add("Irvan Alif");
+daftarMahasiswa.add("Marcela Persa Linthin");
+daftarMahasiswa.add("Alvionej Resna Lawrend Pandiangan");
+daftarMahasiswa.add("Ahmad Dani");
+daftarMahasiswa.add("Elmy Fadillah");
+daftarMahasiswa.add("Dinda Aulia Rizky");
+daftarMahasiswa.add("Muhammad Irdhan Nur Faudzan");
+daftarMahasiswa.add("Ravina Eka Adiya");
+daftarMahasiswa.add("Moch. Farris Alfiansyah");
+daftarMahasiswa.add("Ghendida Gantari Ayari");
+daftarMahasiswa.add("Muhammad Fakhri Al-Kautsar");
+daftarMahasiswa.add("Jihan Alya Naflah");
+daftarMahasiswa.add("Raihan Fariz Novanto");
+daftarMahasiswa.add("Muhammad Farros Anand");
+daftarMahasiswa.add("Indah Maramin Al Inayah");
+daftarMahasiswa.add("Mohamad Ariel Saputra D Loi");
+daftarMahasiswa.add("Aris Candra Muzaffar");
+daftarMahasiswa.add("Rizky Yunia Surono");
+daftarMahasiswa.add("Nabil Daffa Athalasyah");
+daftarMahasiswa.add("Muhammad Fawwaz Ibnu Rasya");
+daftarMahasiswa.add("Nova Rasyadina Anwar");
+daftarMahasiswa.add("Dafa Firdaus");
+daftarMahasiswa.add("Nur Aliyya");
+daftarMahasiswa.add("Yudha Tri Atmaja");
+daftarMahasiswa.add("Chaesarrio Taufiqul Hakim");
+daftarMahasiswa.add("Moreno Ferdinand Farhantino");
+daftarMahasiswa.add("Danial Hirzan Akbary");
+daftarMahasiswa.add("Tsabitah Kawiswara");
+daftarMahasiswa.add("Reswara Ganendra Rashi Dewa");
+daftarMahasiswa.add("Nanda Pesona Putri");
+daftarMahasiswa.add("Luthfi Daffa Purbaya");
+daftarMahasiswa.add("Marza Hanaya Melodya Goga");
+daftarMahasiswa.add("Muhammad Romadhoni Alfatih");
+daftarMahasiswa.add("Muhammad Nur Alfian");
+daftarMahasiswa.add("Muhammad Arifin Alqi. Ab");
+daftarMahasiswa.add("Dimas Aji Mukti");
+daftarMahasiswa.add("Maria Claudia Meo");
+daftarMahasiswa.add("Elvira Agustin");
+daftarMahasiswa.add("Yulius Pune");
+daftarMahasiswa.add("Rizky Wahyu Dina Putri");
+daftarMahasiswa.add("Vebronia Vitania Lusi");
+daftarMahasiswa.add("Ahmad Samsul Arifin");
+daftarMahasiswa.add("Ahmad Qomarul Arifin");
+daftarMahasiswa.add("Keisya Siti Nafisa Andini");
+daftarMahasiswa.add("Jabbar Hafizh Abdillah");
+daftarMahasiswa.add("Muhammad Rifqi Jastiartha");
+daftarMahasiswa.add("Muhammad Rafly");
+daftarMahasiswa.add("Muhamad Irpan Santoso");
+daftarMahasiswa.add("Zyrus Alfredo Randan Malinggato");
+
+        do {
+            System.out.println("\n=== Manajemen Daftar Mahasiswa SI 2024 ===");
+            System.out.println("1. Tambah Mahasiswa");
+            System.out.println("2. Lihat Daftar Mahasiswa");
+            System.out.println("3. Update Data Mahasiswa");
+            System.out.println("4. Hapus Mahasiswa");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = scanner.nextInt();
+            scanner.nextLine(); // membersihkan enter
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan nama mahasiswa: ");
+                    String nama = scanner.nextLine();
+                    daftarMahasiswa.add(nama);
+                    System.out.println("Mahasiswa berhasil ditambahkan!");
+                    break;
+
+                case 2:
+                    System.out.println("\n--- Daftar Mahasiswa SI 2024 ---");
+                    if (daftarMahasiswa.isEmpty()) {
+                        System.out.println("Belum ada data mahasiswa.");
+                    } else {
+                        for (int i = 0; i < daftarMahasiswa.size(); i++) {
+                            System.out.println((i + 1) + ". " + daftarMahasiswa.get(i));
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("\n--- Update Data Mahasiswa ---");
+                    if (daftarMahasiswa.isEmpty()) {
+                        System.out.println("Belum ada data yang bisa diupdate.");
+                    } else {
+                        for (int i = 0; i < daftarMahasiswa.size(); i++) {
+                            System.out.println((i + 1) + ". " + daftarMahasiswa.get(i));
+                        }
+                        System.out.print("Pilih nomor mahasiswa yang ingin diupdate: ");
+                        int idxUpdate = scanner.nextInt();
+                        scanner.nextLine(); 
+                        if (idxUpdate > 0 && idxUpdate <= daftarMahasiswa.size()) {
+                            System.out.print("Masukkan nama baru: ");
+                            String namaBaru = scanner.nextLine();
+                            daftarMahasiswa.set(idxUpdate - 1, namaBaru);
+                            System.out.println("Data berhasil diupdate!");
+                        } else {
+                            System.out.println("Nomor tidak valid!");
+                        }
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("\n--- Hapus Data Mahasiswa ---");
+                    if (daftarMahasiswa.isEmpty()) {
+                        System.out.println("Belum ada data yang bisa dihapus.");
+                    } else {
+                        for (int i = 0; i < daftarMahasiswa.size(); i++) {
+                            System.out.println((i + 1) + ". " + daftarMahasiswa.get(i));
+                        }
+                        System.out.print("Pilih nomor mahasiswa yang ingin dihapus: ");
+                        int idxHapus = scanner.nextInt();
+                        scanner.nextLine();
+                        if (idxHapus > 0 && idxHapus <= daftarMahasiswa.size()) {
+                            String terhapus = daftarMahasiswa.remove(idxHapus - 1);
+                            System.out.println("Mahasiswa " + terhapus + " berhasil dihapus!");
+                        } else {
+                            System.out.println("Nomor tidak valid!");
+                        }
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Terima kasih! Program selesai.");
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+            }
+        } while (pilihan != 5);
+
+        scanner.close();
+    }
+}
